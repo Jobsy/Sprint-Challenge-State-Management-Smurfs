b48bd65a808154702198962d7be2c13234081cf2
Oluwajoba <oluwajobabello@gmail.com>
2019-10-11 09:23:27 +0100

    add Smurfs actionTypes / actionTypes.js / fetch data

--- smurfs/src/states/actionTypes.js
+++ smurfs/src/states/actionTypes.js
@@ -3,3 +3,8 @@
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 export const RESET = "RESET";
+
+
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAIL = 'FETCH_SMURF_FAIL';
