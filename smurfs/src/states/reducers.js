d0fe17b9af1633b3c2d860e81cd1d0039cd717bb
Oluwajoba <oluwajobabello@gmail.com>
2019-10-11 09:29:27 +0100

    add Smurfs reducer function / reducers.js / fetch data

--- smurfs/src/states/reducers.js
+++ smurfs/src/states/reducers.js
@@ -18,3 +18,45 @@ export function counterReducer(count = initialValueCount, action) {
     return  count;
   }
 }
+
+
+
+const initialState = {
+    pokemon: [],
+    error: '',
+    isFetching: false
+  };
+  let initialState = 
+    {
+      name: 'Brainey',
+      age: 200,
+      height: '5cm',
+      id: 0
+    };
+  
+  export function reducer(state = initialState, action) {
+    // console.log('reducer', action);
+    switch (action.type) {
+      case types.FETCH_SMURF_START:
+        return {
+          ...state,
+          isFetching: true,
+          error: ''
+        };
+      case types.FETCH_SMURF_SUCCESS:
+        return {
+          ...state,
+          pokemon: action.payload,
+          isFetching: false,
+          error: ''
+        };
+      case types.FETCH_SMURF_FAIL:
+        return {
+          ...state,
+          error: action.payload
+        };
+      default:
+        return state;
+    }
+  }
+  
